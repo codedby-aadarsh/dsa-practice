cc7d494a7bfe3bff9c56f474aa6e0ed87a3535ff
codedby-aadarsh <aadarshmandal9354@gmail.com>
2025-06-16 18:12:41 +0530

    Time: 55 ms (57.67%), Space: 26.9 MB (63.57%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -3,15 +3,16 @@ class Solution(object):
         if not nums:
             return 0
         longest=0
+        #first you gotta make the list into the set, by doing this we make sure there are no redundant elements in it
         ans=set(nums)
         for i in ans:
-            if i-1 not in ans:
+            if i-1 not in ans:# here we check is there any elements less than the current one by 1. and if this element is already counted. as other sequence start element in the set will have a conisderably gap.
                 length=1
                 curr=i
-                while curr+1 in ans:
-                    curr+=1
+                while curr+1 in ans: # checking the consecutive element
+                    curr+=1#incrementing the current 
                     length+=1
-                longest=max(longest,length)
+                longest=max(longest,length)#comparing with the past longest sequence 
         return longest
 
         
