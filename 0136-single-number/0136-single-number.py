dc947d60d648510a5db6f661a8832397b976aeed
codedby-aadarsh <aadarshmandal9354@gmail.com>
2025-06-20 13:39:06 +0530

    Time: 7 ms (45.81%), Space: 14.7 MB (19.16%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -8,5 +8,12 @@ class Solution(object):
         sum2=set(nums)
         return 2*sum(sum2)-sum1
 
+        # e.g= a=[a,a,b,b,c]  logic 
+        # sum1=2a+2b+c
+        # sum2=[a,b,c]
+        # sum2=a+b+c
+        # 2*sum2-sum1= 2a+2b+2c-(2a+2b+c)=c
+
+
             
         
