89af489f96a1015cfa881bc89c95e3f3376106bd
codedby-aadarsh <aadarshmandal9354@gmail.com>
2025-07-15 09:53:54 +0530

    Time: 0 ms (100%), Space: 18 MB (8.51%) - LeetHub

--- 0066-plus-one/0066-plus-one.py
+++ 0066-plus-one/0066-plus-one.py
@@ -1,13 +1,31 @@
 class Solution:
-    def plusOne(self, dig: List[int]) -> List[int]:
-        for pos in range(len(dig)-1,-1,-1):
-            if dig[pos]<9:
-                dig[pos]+=1
-                return dig
-            elif dig[pos]>=9:
-                dig[pos]=0
-        return [1]+dig
+    def plusOne(self, digits: List[int]) -> List[int]:
+        # if len(digits)==0:
+        #     return [1]
+        # elif digits[-1]!=9:
+        #     digits[-1]+=1
+        #     return digits
+        # else:
+        #     res=[]
+        #     carr=0
+        #     digits[-1]+=1
+        #     for i in digits[::-1]:
+        #         res.append((i+carr)%10)
+        #         carr=(i+carr)//10
+        #     if carr:
+        #         res.append(carr)
+        #     return res[::-1]
+        n = len(digits)
+        for i in range(n - 1, -1, -1):
+            if digits[i] < 9:
+                digits[i] += 1
+                return digits
+            digits[i] = 0
 
-                
+        # If we reach here, it means all digits were 9
+        # We need to add a new digit at the beginning
+        return [1] + digits
 
-    
+            
+
+
